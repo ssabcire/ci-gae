6960abba4787a684fc01bc047c93bad7a4c6865a
ssab <qasw4554@gmail.com>
2020-03-14 16:39:15 +0900

    first commit

---
+++ hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("occur error in ListenAndServe. err: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
